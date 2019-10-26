fc6d2f4a3dee066ae82e10453362bf0cdabb89ec
Nyx <nichole.steig@gmail.com>
2019-10-26 13:58:59 -0700

    code from end of class 10/26

--- drinkscript.js
+++ drinkscript.js
@@ -1,33 +1,54 @@
-$("#drink-button").on("click", function () { 
-    let ingredient = $("#textarea-drink").val();
+$("#drink-button").on("click", function () {
+  let ingredient = $("#textarea-drink").val();
   findDrinkByIngredient(ingredient);
-  var cocktailID = "15346";
-  var cocktailURL = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=" + cocktailID;
-  
+
 });
-  
-function findDrinkByIngredient(ingredient) { 
-    var ingredientURL = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=" + ingredient;
-$.ajax({
-      url: ingredientURL,
-      method: "GET"
-    }).then(function(response) {
-      console.log(response);
-      for (var i=0; i < 5; i++){
-          var IDnum = response.drinks[i].idDrink;
-          console.log(IDnum);
-          findRecipe(IDnum);
-      }
-    });
+
+function findDrinkByIngredient(ingredient) {
+  var ingredientURL = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=" + ingredient;
+  $.ajax({
+    url: ingredientURL,
+    method: "GET"
+  }).then(function (response) {
+    console.log(response);
+    for (var i = 0; i < 5; i++) {
+      var IDnum = response.drinks[i].idDrink;
+      console.log(IDnum);
+      findRecipe(IDnum);
+    }
+  });
 };
 
-function findRecipe(IDnum){
-var cocktailURL = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=" + IDnum;
-    $.ajax({
-        url: cocktailURL,
-        method: "GET"
-    }).then(function(response){
-  console.log(response);
-    });
-};
+function findRecipe(IDnum) {
+  var cocktailURL = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=" + IDnum;
+  $.ajax({
+    url: cocktailURL,
+    method: "GET"
+  }).then(function (response) {
+    console.log(response);
+    var drinkName = response.drinks[0].strDrink;
+    console.log(drinkName);
+    var instruction = response.drinks[0].strInstructions;
+    console.log(instruction);
+    var drinkData = response.drinks[0];
+    for (var i = 0; i <drinkData.strIngredient[i]; i++) {
+      var ingredientArray = drinkData.strIngredient[i];
+      console.log(ingredientArray);
+    }
+    //var ingArray = [drinkData.strIngredient1, drinkData.strIngredient2, drinkData.strIngredient3, ];
+    //console.log(ingArray);
+    
+// var ingredient = drinkData.forEach
+// if (ingredient != null) {
+// return ingredient;
+// console.log(ingredient);
 
+//     };
+    
+    //for (var i = 0; i < 15; i ++){
+    //   var ingArray = 
+    // }
+    //var ingredients = response.
+    //var measurements = response. (array)
+  });
+};
